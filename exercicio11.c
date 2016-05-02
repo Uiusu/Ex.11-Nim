22b4e6ea7266c5590b119b82f8f5a041cfde9498
Jose Wilson Cavalcante Ferreira Junior <jwilsoncfj@gmail.com>
2016-05-01 23:42:56 -0300

    Coloquei o xor e ajeitei alguns erros na equacao do u

--- exercicio11.c
+++ exercicio11.c
@@ -10,71 +10,131 @@ int main ()
     scanf("%d", &x);
     c= x/100;
     d= (x-(c*100))/10;
-    u= x-(c*100)+(d*10);
+    u= x-((c*100)+(d*10));
 
-    if(c==1 && d==2*u) // inicio bug xor
-        printf("%d%d%d", c,d-1,u);
+    if(c==1 && d==3 && u==6) // inicio bug xor
+        printf("%d%d%d\n", c,d,u-2);
     else
     {
-        if(c==1 && u==2*d)
-            printf("%d%d%d",c,d,u-1);
+        if(c==1 && d==2*u)
+            printf("%d%d%d\n", c,d-1,u);
         else
         {
-            if (c==1 && u==1)
-                printf("%d%d%d",c,d-(d-1),u);
+            if(c==1 && u==2*d)
+                printf("%d%d%d\n",c,d,u-1);
             else
             {
-                if(c==1 && d==1)
-                    printf("%d%d%d",c,d,u-(u-1));
+                if (c==1 && u==1)
+                    printf("%d%d%d\n",c,d-(d-1),u);
                 else
                 {
-                    if(d==0 && u==1)
-                        printf("%d%d%d", c-c,d,u);
+                    if(c==1 && d==1)
+                        printf("%d%d%d\n",c,d,u-(u-1));
                     else
                     {
-                        if(d==1 && u==1)
-                            printf("%d%d%d",c-c,d,u);
+                        if(d==0 && u==1)
+                            printf("%d%d%d\n", c-c,d,u);
                         else
                         {
-                            if(c==0 && u==1)
-                                printf("%d%d%d",c,d-d,u);
+                            if(d==1 && u==1)
+                                printf("%d%d%d\n",c-c,d,u);
                             else
                             {
-                                if(c==1 && u==0)
-                                    printf("%d%d%d",c,d-d,u);
+                                if(c==0 && u==1)
+                                    printf("%d%d%d\n",c,d-d,u);
                                 else
                                 {
-                                    if(c==0 && d==1)
-                                        printf("%d%d%d",c,d,u-u);
+                                    if(c==1 && u==0)
+                                        printf("%d%d%d\n",c,d-d,u);
                                     else
                                     {
-                                        if(c==1 && d==0)
-                                            printf("%d%d%d",c,d,u-u);
+                                        if(c==0 && d==1)
+                                            printf("%d%d%d\n",c,d,u-u);
                                         else
                                         {
-                                            if((u^d^c)==1)
+                                            if(c==1 && d==0)
+                                                printf("%d%d%d\n",c,d,u-u);
+                                            else
                                             {
-                                                if(c>0)
-                                                    printf("%d%d%d",c-1,d,u);
+                                                if((u^d^c)==1)
+                                                {
+                                                    if(c>0)
+                                                        printf("%d%d%d\n",c-1,d,u);
+                                                    else
+                                                    {
+                                                        if(d>0)
+                                                            printf("%d%d%d\n",c,d-1,u);
+                                                    }
+                                                }
+
                                                 else
                                                 {
-                                                    if(d>0)
-                                                        printf("%d%d%d",c,d-1,u);
+                                                    if((u^d^c)==2)
+                                                    {
+                                                        if(c>1)
+                                                            printf("%d%d%d\n",c-2,d,u);
+                                                        else
+                                                        {
+                                                            printf("%d%d%d\n",c,d-2,u);
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        if((u^d^c)==3)
+                                                        {
+                                                            if(c>2)
+                                                                printf("%d%d%d\n",c-3,d,u);
+                                                            else
+                                                            {
+                                                                if(d>2)
+                                                                    printf("%d%d%d\n",c,d-3,u);
+                                                            }
+                                                        }
+                                                        else
+                                                        {
+                                                            if((u^d^c)==4)
+                                                            {
+                                                                if(d>3)
+                                                                    printf("%d%d%d\n",c,d-4,u);
+                                                                else
+                                                                {
+                                                                    if(u>3)
+                                                                        printf("%d%d%d\n",c,d,u-4);
+                                                                }
+                                                            }
+                                                            else
+                                                            {
+                                                                if((u^d^c)==5)
+                                                                {
+                                                                    if(d>4)
+                                                                        printf("%d%d%d\n",c,d-5,u);
+                                                                    else
+                                                                    {
+                                                                        if(u>4)
+                                                                            printf("%d%d%d\n",c,d,u-5);
+                                                                    }
+                                                                }
+                                                                if((u^d^c)==6)
+                                                                {
+                                                                    if(d>5)
+                                                                        printf("%d%d%d\n",c,d,u-6);
+                                                                }
+
+                                                            }
+                                                        }
+                                                    }
                                                 }
                                             }
                                         }
-                                        else
-                                        {
-                                            if((u^d^c)==2)
-                                            {
-                                                if(c>1)
-                                                    printf("%d%d%d",c-2,d,u);
-                                                else
-                                                {
-                                                    printf("%d%d%d",c,d-2,u);
-                                                }
-                                            }
-
-
-
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return EXIT_SUCCESS;
 }
+
